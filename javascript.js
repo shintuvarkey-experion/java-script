e4177c9cb2cc1396ae38664cde8af6481f73148a
shintuvarkey <shintu.varkey@experionglobal.com>
2017-02-07 14:13:01 +0530

    commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function changes() {
+		document.getElementById('myImage').src='bulb2.jpg';
+		document.getElementById('myImage').src='bulb.jpg';
+		document.getElementById('demo1').style.fontSize='100px';
+		document.getElementById('demo2').style.display='none';
+		document.getElementById('demo3').style.display='block';
+			}
---
+++ javascript1.js
@@ -0,0 +1,12 @@
+function changes() {
+		document.getElementById('myImage').src='bulb2.jpg';
+	}
+function changes2(){
+		document.getElementById('myImage').src='bulb.jpg';
+	}
+		
+function changes1(){
+		document.getElementById('demo1').style.fontSize='100px';
+		document.getElementById('demo2').style.display='none';
+		document.getElementById('demo3').style.display='block';
+}
